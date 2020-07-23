c096ec704dc98369124809fef655510b54e5e310
BraydonKains <kainsbraydon@gmail.com>
2020-07-23 12:04:08 -0400

    Wrote tests for the admin UI table, hope the tests run in CI

---
+++ test/utilities/tccTestService.js
@@ -0,0 +1,15 @@
+export class TccTestService {
+	constructor(patches) {
+		this.getAssociations = async() => [];
+
+		this.getDepartments = async() => [];
+
+		this.getRoles = async() => [];
+
+		if (patches) {
+			for (const [functionName, patch] of Object.entries(patches)) {
+				this[functionName] = patch;
+			}
+		}
+	}
+}
