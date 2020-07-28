6b8a1459efa0694390ef04a9b4be389da9971a76
Joseph Wong <joseph.wong@d2l.com>
2020-07-28 13:16:54 -0400

    US116068 TA145479 build get route wrappers for tcc service

--- src/services/tccService.js
+++ src/services/tccService.js
@@ -1,13 +1,42 @@
+import { Routes } from './routes';
+
 export class TccService {
+	static _getRequest(url) {
+		console.log(url);
+		return fetch(url, this._options('GET')).then(r => r.json());
+	}
+	static _options(method) {
+		return {
+			credentials: 'include',
+			headers: new Headers({
+				'Access-Control-Allow-Origin': '*',
+			}),
+			method,
+			mode: 'cors'
+		};
+	}
 	static async getAssociations() {
-
+		return await this._getRequest(Routes.CourseConfig());
 	}
-
 	static async getDepartments() {
-
+		const departmentInfo = await this._getRequest(Routes.DepartmentInfo());
+		let bookmark = null;
+		let departments = [];
+		do {
+			const body = await this.getPagedDepartments(departmentInfo.Id, bookmark);
+			const pageInfo = body.PagingInfo;
+			departments = departments.concat(body.Items);
+			bookmark = pageInfo.Bookmark;
+			if (!pageInfo.HasMoreItems) {
+				break;
+			}
+		} while (bookmark);
+		console.log(departments);
 	}
-
+	static async getPagedDepartments(departmentTypeId, bookmark) {
+		return await this._getRequest(Routes.Departments(departmentTypeId, bookmark));
+	}
 	static async getRoles() {
-
+		return this._getRequest(Routes.Roles());
 	}
 }
